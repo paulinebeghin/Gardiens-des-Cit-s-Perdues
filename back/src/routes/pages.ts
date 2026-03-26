ef1840645f8b5d84c27196d3ad67e7b9f5fc4030
pauline beghin <paulinebeghin7@gmail.com>
2026-03-26 16:36:22 +0100

    :sparkles:

--- back/src/routes/pages.ts
+++ back/src/routes/pages.ts
@@ -130,19 +130,17 @@
 
 // export default router;
 
-
 import { authMiddleware } from "@/middlewares/auth.middleware";
 import express from "express";
 import * as characterController from "@/controllers/character.controller";
 
 const router: express.Router = express.Router();
 
-router.use(authMiddleware);
 
 router.get("/", characterController.getAll)
 router.get("/:slug", characterController.getBySlug)
-router.patch("/:slug", characterController.update)
-router.post("/:id", characterController.create)
-router.delete("/:id", characterController.create)
+router.patch("/:slug", authMiddleware, characterController.update)
+router.post("/:id", authMiddleware,  characterController.create)
+router.delete("/:id", authMiddleware,  characterController.create)
 
 export default router
