398acb18d6b5ce82ce96035398f6d8975093d1ed
pauline beghin <paulinebeghin7@gmail.com>
2026-04-06 18:23:52 +0200

    tsconfig.build.json modification

--- front/src/api/book.ts
+++ front/src/api/book.ts
@@ -6,7 +6,7 @@ export const useFetchBook = (id: string | undefined) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
   useEffect(() => {
     
     if (!id) return;
