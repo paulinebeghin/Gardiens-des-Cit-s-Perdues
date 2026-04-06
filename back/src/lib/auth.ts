133f7d99756adf0b188a924cff10b2081a724fcb
pauline beghin <paulinebeghin7@gmail.com>
2026-04-06 19:06:25 +0200

    tsconfig.build.json modification

--- back/src/lib/auth.ts
+++ back/src/lib/auth.ts
@@ -4,6 +4,7 @@ import db from "@/lib/db";
 
 const isProduction = process.env.NODE_ENV === "production";
 
+
 export const auth = betterAuth({
   database: prismaAdapter(db, {
     provider: "postgresql",
@@ -11,35 +12,17 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  account: {
-    accountLinking: {
-      enabled: true,
-
-      trustedProviders: ["google", "github"] 
-    }
-  },
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
     github: {
-  clientId: process.env.GITHUB_CLIENT_ID!,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-  // CETTE FONCTION FORCE L'EMAIL SI GITHUB NE LE DONNE PAS
-  mapUser: (user: { email: any; id: any; }) => ({
-    ...user,
-    email: user.email || `${user.id}@github.com`,
-  }),
-},
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
   },
-
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:8000",
-   trustedOrigins: [
-    "http://localhost:5173", 
-  
-    "https://gardiens-des-cit-s-perdues.vercel.app"
-],
+  trustedOrigins: [process.env.FRONTEND_URL || "http://localhost:5173"],
   advanced: {
     crossSubDomainCookies: {
       enabled: isProduction,
