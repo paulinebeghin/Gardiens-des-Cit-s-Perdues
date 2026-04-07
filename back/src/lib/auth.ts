debebf0b8747c6d6025b2abc5d2c18e706c788fd
pauline beghin <paulinebeghin7@gmail.com>
2026-04-07 11:11:50 +0200

    tsconfig.build.json modification

--- back/src/lib/auth.ts
+++ back/src/lib/auth.ts
@@ -5,10 +5,8 @@ import db from "@/lib/db";
 const isProduction = process.env.NODE_ENV === "production";
 const baseURL = process.env.BETTER_AUTH_URL || (isProduction ? "https://gardiens-des-cit-s-perdues-production.up.railway.app" : "http://localhost:8000");
 
-export const auth = betterAuth({
-  baseURL,
-  basePath: "/api/auth",
 
+export const auth = betterAuth({
   database: prismaAdapter(db, {
     provider: "postgresql",
   }),
@@ -19,32 +17,20 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      redirectURI: `${baseURL}/api/auth/callback/google`,
     },
     github: {
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-      redirectURI: `${baseURL}/api/auth/callback/github`,
     },
   },
-  trustedOrigins: [
-  "http://localhost:5173",
-  "http://localhost:8000",
-  "https://gardiens-des-cit-s-perdues.vercel.app",
-  "https://gardiens-des-cit-s-perdues-production.up.railway.app"
-],
+  trustedOrigins: [process.env.FRONTEND_URL || "http://localhost:5173"],
   advanced: {
     crossSubDomainCookies: {
       enabled: isProduction,
     },
-    
-    useSecureCookies: isProduction,
     defaultCookieAttributes: {
       secure: isProduction,
       sameSite: isProduction ? "none" : "lax",
     },
-    redirectTo: isProduction 
-        ? "https://gardiens-des-cit-s-perdues.vercel.app/dashboard" 
-        : "http://localhost:5173/dashboard",
   },
 });
