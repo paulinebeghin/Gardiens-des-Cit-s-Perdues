fac00021ce2075f491bf0752e7476e306e754cfd
pauline beghin <paulinebeghin7@gmail.com>
2026-04-07 12:35:41 +0200

    tsconfig.build.json modification

--- back/src/index.ts
+++ back/src/index.ts
@@ -16,7 +16,7 @@ const allowedOrigins = ["http://localhost:5173", "https://gardiens-des-cit-s-per
 if (process.env.FRONTEND_URL) allowedOrigins.push(process.env.FRONTEND_URL);
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: allowedOrigins,
   credentials: true,
 }));
 
